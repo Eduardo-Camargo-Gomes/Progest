18c5fdfac15dcd5f4de5afae2b260a3cda41af2e
Eduardo <edu.c.gomes.04@gmail.com>
2023-09-04 23:39:59 -0300

    Verificação dos campos nome e cpf

--- Progest/src/controller/UsuarioController.java
+++ Progest/src/controller/UsuarioController.java
@@ -8,71 +8,51 @@ import model.*;
 import model.DAO.*;
 import view.*;
 import java.util.*;
-
 import javax.swing.JOptionPane;
 
 public class UsuarioController {
-    
-
-   
-  
-	public boolean cadastrarUsuario(String nome, String senha, String cpf) throws SQLException {
-
-            boolean sim;
-            UsuarioDAO usuarioDAO = new UsuarioDAO();
-
-         UsuarioModel usuarioACadastrar = new UsuarioModel(nome, senha, cpf);
-         
-           sim = usuarioDAO.verificarSeExisteUsuario(usuarioACadastrar);
+    public boolean cadastrarUsuario(String nome, String senha, String cpf) throws SQLException {
+        boolean sim;
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        UsuarioModel usuarioACadastrar = new UsuarioModel(nome, senha, cpf);
+        sim = usuarioDAO.verificarSeExisteUsuario(usuarioACadastrar);
  
-      if(nome.length() > 0 && senha.length() > 0 && cpf.length() > 0 ){
-          if(sim == true){
-              
-              JOptionPane.showMessageDialog(null, "Nome de usuario já existente!");
-          }// fim if 
-          
-          else {
-          usuarioDAO.cadastrarUsuario(usuarioACadastrar);
-          
-          JOptionPane.showMessageDialog(null,"Usuário cadastrado com sucesso!" );
-          return true;
-          }// fim else de nome ja existente 
-          
-      } // fim if 
-      else {
-          
-          
-          
-          JOptionPane.showMessageDialog(null, "Campos não preenchidos corretamente!");
-          
-      }// fim else de os campos nao forem preenchidos
- 
-	return false;
-	}// fim metodo 
-        
-     
-        
-        public boolean Autenticar(String nome, String senha) throws SQLException{
-            
-            UsuarioModel usuarioAFazerLogin = new UsuarioModel(nome,senha);
-           
-            UsuarioDAO usuarioDAO = new UsuarioDAO();
-
- usuarioDAO.ExisteAutenticacao(usuarioAFazerLogin);
-              
-         boolean sucessoNaAutenticacao = (usuarioDAO.ExisteAutenticacao(usuarioAFazerLogin));
-             
-             if(sucessoNaAutenticacao){
-             
-               return true;
-             }// fim if
-             
-             else {
+        // Verifique se os campos obrigatórios não estão vazios
+        if (nome.length() > 0 && senha.length() > 0 && cpf.length() > 0) {
+            if (sim == true) {
+                JOptionPane.showMessageDialog(null, "Nome de usuário já existente!");
+            } else {
                 
-                 return false;
-             }
-         
+                if (!nome.matches("[A-Za-z]+")) {
+                    JOptionPane.showMessageDialog(null, "O campo nome deve conter apenas letras.");
+                } 
              
-        }// fim metodo
-	
-}// fim classe 
+                else if (!cpf.matches("[0-9]+")) {
+                    JOptionPane.showMessageDialog(null, "O campo CPF deve conter apenas números.");
+                } else {
+                    usuarioDAO.cadastrarUsuario(usuarioACadastrar);
+                    JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!");
+                    return true;
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Campos não preenchidos corretamente!");
+        }
+        return false;
+    }
+    
+    public boolean Autenticar(String nome, String senha) throws SQLException {
+        UsuarioModel usuarioAFazerLogin = new UsuarioModel(nome, senha);
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        
+        usuarioDAO.ExisteAutenticacao(usuarioAFazerLogin);
+        
+        boolean sucessoNaAutenticacao = usuarioDAO.ExisteAutenticacao(usuarioAFazerLogin);
+        
+        if (sucessoNaAutenticacao) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
